3bd2421772d6a72aff35bbdd09fe593a61e38d86
Sunny Bharadwaj <sunnyb1597@gmail.com>
2020-08-31 15:50:56 +0530

    css scaffolding in progress

--- public/js/app.js
+++ public/js/app.js
@@ -37331,8 +37331,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/rebecca/Development/projects/work/MyPortfolio/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/rebecca/Development/projects/work/MyPortfolio/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/sunny/Projects/MyPortfolio-v1/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/sunny/Projects/MyPortfolio-v1/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
